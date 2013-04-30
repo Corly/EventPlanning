01bf771f3c759961e67388b1e96fb36e9523982e
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2013-04-30 21:22:53 +0300

    Delete the unused

--- android/src/com/smartIntern/server/JsonReadable.java
+++
@@ -1,8 +0,0 @@
-package com.smartIntern.server;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public interface JsonReadable {
-	public void readFromJson(JSONObject jsonObject) throws JSONException;
-}
