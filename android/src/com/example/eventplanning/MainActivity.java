685265b74035fa512d305791cec373fef45e96ac
Botezatu Mihai Catalin <botezatu.mihaicatalin@gmail.com>
2013-05-02 18:57:10 +0300

    LFHJASGJF

--- android/src/com/example/eventplanning/MainActivity.java
+++ android/src/com/example/eventplanning/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends Activity
 					i.putExtra("category", category );
 					i.putExtra("radius", radius);
 					startActivity(i);
+					//ghdghdsjgjksdhj
 				}
 				
 			}
