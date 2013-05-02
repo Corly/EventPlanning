b394322076d0ab9caf36e08820a6ba900524ef02
Botezatu Mihai Catalin <botezatu.mihaicatalin@gmail.com>
2013-05-02 19:00:55 +0300

    MATA!

--- android/src/com/example/eventplanning/MainActivity.java
+++ android/src/com/example/eventplanning/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends Activity
 					i.putExtra("category", category );
 					i.putExtra("radius", radius);
 					startActivity(i);
-					//ghdghdsjgjksdhj
 				}
 				
 			}
