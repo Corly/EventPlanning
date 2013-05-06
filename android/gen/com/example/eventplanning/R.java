5dbaf779519456ced452a22ee27a2712f03e4ec9
Mihai <mihai@mihai-SATELLITE-C855-12Z.(none)>
2013-05-06 21:41:57 +0300

    Changed highlights color and style

--- android/gen/com/example/eventplanning/R.java
+++ android/gen/com/example/eventplanning/R.java
@@ -9,21 +9,24 @@ package com.example.eventplanning;
 
 public final class R {
     public static final class array {
-        public static final int cities_list=0x7f060001;
-        public static final int poi_categories=0x7f060000;
+        public static final int cities_list=0x7f070001;
+        public static final int poi_categories=0x7f070000;
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int orange=0x7f040000;
+    }
     public static final class dimen {
-        public static final int Dimension=0x7f040002;
+        public static final int Dimension=0x7f050002;
         /**  Default screen margins, per the Android Design guidelines. 
 
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int button=0x7f020000;
@@ -34,51 +37,52 @@ public final class R {
         public static final int close2=0x7f020005;
         public static final int ic_launcher=0x7f020006;
         public static final int images=0x7f020007;
-        public static final int listselector=0x7f020008;
-        public static final int mapwasnotloaded=0x7f020009;
-        public static final int mataglobe=0x7f02000a;
-        public static final int nimic=0x7f02000b;
+        public static final int listselect=0x7f020008;
+        public static final int listselector=0x7f020009;
+        public static final int mapwasnotloaded=0x7f02000a;
+        public static final int mataglobe=0x7f02000b;
+        public static final int nimic=0x7f02000c;
     }
     public static final class id {
-        public static final int RouteTextView1=0x7f090006;
-        public static final int RouteTextView2=0x7f090005;
-        public static final int btn_fav=0x7f090014;
-        public static final int btn_find=0x7f090016;
-        public static final int choose=0x7f090000;
-        public static final int clear_all_destinations=0x7f09000e;
-        public static final int clear_all_routes=0x7f09001d;
-        public static final int current_dest_poi_name=0x7f09001e;
-        public static final int current_dest_tag=0x7f09000d;
-        public static final int edit_radius=0x7f090003;
-        public static final int find=0x7f090004;
-        public static final int find_pois=0x7f090012;
-        public static final int hint_city=0x7f090013;
-        public static final int hint_find_location=0x7f090015;
-        public static final int horizontalScrollView1=0x7f090007;
-        public static final int imageView1=0x7f09000a;
-        public static final int listBox1=0x7f090009;
-        public static final int list_enter_routes_name=0x7f09001f;
-        public static final int list_saved_routes_name=0x7f090022;
-        public static final int make_route=0x7f090026;
-        public static final int menu_fav_points=0x7f090025;
-        public static final int menu_save_point=0x7f090023;
-        public static final int menu_saved_routes=0x7f090024;
-        public static final int poi_activitity_category_tag=0x7f090017;
-        public static final int progress_bar=0x7f090018;
-        public static final int radius=0x7f090002;
-        public static final int restaurant_address=0x7f090021;
-        public static final int restaurant_name=0x7f090020;
-        public static final int route_progress=0x7f09000b;
-        public static final int save_route=0x7f09000c;
-        public static final int saved_route_distance=0x7f09001a;
-        public static final int saved_route_tag=0x7f090019;
-        public static final int saved_route_time=0x7f09001b;
-        public static final int show_current=0x7f090008;
-        public static final int show_route=0x7f09000f;
-        public static final int spinner1=0x7f090001;
-        public static final int title=0x7f090011;
-        public static final int txt_saved=0x7f09001c;
-        public static final int welcome=0x7f090010;
+        public static final int RouteTextView1=0x7f0a0006;
+        public static final int RouteTextView2=0x7f0a0005;
+        public static final int btn_fav=0x7f0a0014;
+        public static final int btn_find=0x7f0a0016;
+        public static final int choose=0x7f0a0000;
+        public static final int clear_all_destinations=0x7f0a000e;
+        public static final int clear_all_routes=0x7f0a001d;
+        public static final int current_dest_poi_name=0x7f0a001e;
+        public static final int current_dest_tag=0x7f0a000d;
+        public static final int edit_radius=0x7f0a0003;
+        public static final int find=0x7f0a0004;
+        public static final int find_pois=0x7f0a0012;
+        public static final int hint_city=0x7f0a0013;
+        public static final int hint_find_location=0x7f0a0015;
+        public static final int horizontalScrollView1=0x7f0a0007;
+        public static final int imageView1=0x7f0a000a;
+        public static final int listBox1=0x7f0a0009;
+        public static final int list_enter_routes_name=0x7f0a001f;
+        public static final int list_saved_routes_name=0x7f0a0022;
+        public static final int make_route=0x7f0a0026;
+        public static final int menu_fav_points=0x7f0a0025;
+        public static final int menu_save_point=0x7f0a0023;
+        public static final int menu_saved_routes=0x7f0a0024;
+        public static final int poi_activitity_category_tag=0x7f0a0017;
+        public static final int progress_bar=0x7f0a0018;
+        public static final int radius=0x7f0a0002;
+        public static final int restaurant_address=0x7f0a0021;
+        public static final int restaurant_name=0x7f0a0020;
+        public static final int route_progress=0x7f0a000b;
+        public static final int save_route=0x7f0a000c;
+        public static final int saved_route_distance=0x7f0a001a;
+        public static final int saved_route_tag=0x7f0a0019;
+        public static final int saved_route_time=0x7f0a001b;
+        public static final int show_current=0x7f0a0008;
+        public static final int show_route=0x7f0a000f;
+        public static final int spinner1=0x7f0a0001;
+        public static final int title=0x7f0a0011;
+        public static final int txt_saved=0x7f0a001c;
+        public static final int welcome=0x7f0a0010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -94,19 +98,19 @@ public final class R {
         public static final int simple_saved_routes_list=0x7f03000a;
     }
     public static final class menu {
-        public static final int current_destination=0x7f080000;
-        public static final int front=0x7f080001;
-        public static final int main=0x7f080002;
+        public static final int current_destination=0x7f090000;
+        public static final int front=0x7f090001;
+        public static final int main=0x7f090002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int cities_choose=0x7f050007;
-        public static final int hello_world=0x7f050002;
-        public static final int hint1=0x7f050004;
-        public static final int hint2=0x7f050005;
-        public static final int poi_choose=0x7f050006;
-        public static final int title_activity_route=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int cities_choose=0x7f060007;
+        public static final int hello_world=0x7f060002;
+        public static final int hint1=0x7f060004;
+        public static final int hint2=0x7f060005;
+        public static final int poi_choose=0x7f060006;
+        public static final int title_activity_route=0x7f060003;
     }
     public static final class style {
         /** 
@@ -119,10 +123,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
